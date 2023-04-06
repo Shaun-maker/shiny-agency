5d35e2ab3ca1f53c222093069d5e4f125a773cf5
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-06 11:56:18 +0200

    create some route

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Header from './components/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+  },
+  {
+    path: '/survey',
+    element: <Survey />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Home />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
